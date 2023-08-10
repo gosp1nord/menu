e8a9885c1661e92c460c0d8396c6c53d43f0f4aa
Yuriy N <gosp1nord@gmail.com>
2023-08-10 14:49:14 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Menu, HomePage, DriftPage, TimeAttackPage, ForzaPage } from './components/pages';
+import { Routes, Route } from 'react-router';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='block-app'>
+      <Menu />
+      <div className="page">
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/drift" element={<DriftPage />} />
+        <Route path="/timeattack" element={<TimeAttackPage />} />
+        <Route path="/forza" element={<ForzaPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
-
-export default App;
